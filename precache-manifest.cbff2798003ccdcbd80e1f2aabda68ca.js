cf697813f9689edd5c1c2842a7d0c3652a7e2895
A-DEEB <amd.deeb@outlook.com>
2020-04-02 15:24:33 -0400

    Updates

---
+++ precache-manifest.cbff2798003ccdcbd80e1f2aabda68ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b349007c20cd6ac2f30344a64ab79e",
+    "url": "/yugioh-db/index.html"
+  },
+  {
+    "revision": "ff7ec36ba3b8a0f64314",
+    "url": "/yugioh-db/static/css/main.83e1afe8.chunk.css"
+  },
+  {
+    "revision": "5f6ae762b2e85da0ffa0",
+    "url": "/yugioh-db/static/js/2.a9e04bff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yugioh-db/static/js/2.a9e04bff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff7ec36ba3b8a0f64314",
+    "url": "/yugioh-db/static/js/main.18cfccf6.chunk.js"
+  },
+  {
+    "revision": "ce1947bee80679c2c406",
+    "url": "/yugioh-db/static/js/runtime-main.8df8ffb4.js"
+  }
+]);
